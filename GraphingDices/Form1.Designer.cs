663118f50a172758c6e8f35536a09ca1d0d0f68c
WillMon <w.montero34@gmail.com>
2016-03-14 18:17:19 -0500

    Init Commit
    Setting up the probability of the chances to get a set number from dices

---
+++ GraphingDices/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace GraphingDices
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Roll = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Roll)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Roll
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.Roll.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.Roll.Legends.Add(legend2);
+            this.Roll.Location = new System.Drawing.Point(12, 12);
+            this.Roll.Name = "Roll";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Damage Roll";
+            series2.YValuesPerPoint = 3;
+            this.Roll.Series.Add(series2);
+            this.Roll.Size = new System.Drawing.Size(470, 300);
+            this.Roll.TabIndex = 0;
+            this.Roll.Text = "chat1";
+            this.Roll.Click += new System.EventHandler(this.chart1_Click);
+            this.Roll.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Roll_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Roll";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(507, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(82, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 597);
+            this.ControlBox = false;
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Roll);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Roll)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart Roll;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ GraphingDices/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphingDices
+{
+    public partial class Form1 : Form
+    {
+        int count = 0, total = 0, avrage;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(avrage.ToString());
+        }
+
+        private void Roll_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Roll.ChartAreas[0].AxisY.Maximum = 12;
+            Roll.ChartAreas[0].AxisY.Minimum = 0;
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            List<int> AndrewOneSidedDiceRoll = new List<int> { 4, 2, 2, 5, 5, 2, 5, 6, 2,
+             5, 4, 5, 3, 3, 5, 1, 5, 3, 2, 6 };
+            //AndrewOneSidedDiceRoll.Sort();
+            foreach(int i in AndrewOneSidedDiceRoll)
+            {
+                Roll.Series["Damage Roll"].Points.AddY(i);
+                ++count;
+                total += i;
+            }
+            avrage = total / count;
+        }
+    }
+}
